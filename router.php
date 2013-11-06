e5d2311c4b21dad9774554b0ecdf22a3c564792d
Mike <korolev_m@ukr.net>
2013-11-06 11:09:41 -0500

    add header

--- router.php
+++ router.php
@@ -1,6 +1,6 @@
 <?php
     $url="http://learn.knockoutjs.com";
     $req = $_SERVER['REQUEST_URI'];
-
+    header('Content-Type: application/json');
     echo json_encode(file_get_contents($url.$req));
 ?>
